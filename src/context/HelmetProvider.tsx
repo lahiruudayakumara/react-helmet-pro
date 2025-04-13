0e71bae6a95f4e4479c96c57005b33f76c2a2fff
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2025-04-13 13:10:28 +0530

    add HelmetProvider component for managing head state and context

---
+++ src/context/HelmetProvider.tsx
@@ -0,0 +1,21 @@
+import React, { useMemo, useState } from "react";
+
+import { HelmetContext } from "./HelmetContext";
+
+export const HelmetProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [head, setHead] = useState<{
+    title?: string;
+    meta?: { name: string; content: string }[];
+  }>({});
+  const contextValue = useMemo(
+    () => ({ setHead, title: head.title, meta: head.meta }),
+    [setHead, head.title, head.meta]
+  );
+  return (
+    <HelmetContext.Provider value={contextValue}>
+      {children}
+    </HelmetContext.Provider>
+  );
+};
