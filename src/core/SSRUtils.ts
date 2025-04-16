58802654c254e802db9c3f8b72fa1447c73ba4ff
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2025-04-16 23:45:00 +0530

    rename collectHeadTags to collectHelmetTags for consistency

--- src/core/SSRUtils.ts
+++ src/core/SSRUtils.ts
@@ -1,5 +1,5 @@
 import { JSX } from "react";
 
-export const collectHeadTags = (elements: JSX.Element[]) => {
+export const collectHelmetTags = (elements: JSX.Element[]) => {
   return elements.map((el, index) => ({ ...el, key: `head-tag-${index}` }));
 };
