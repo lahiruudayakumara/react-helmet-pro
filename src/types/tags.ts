8881b98667296b580c1aca072c21760b3108c029
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2025-06-17 23:35:16 +0530

    update HELMET_TAGS to include 'base' and 'htmlAttributes'; modify MetaTag interface for optional name and property

--- src/types/tags.ts
+++ src/types/tags.ts
@@ -1,9 +1,13 @@
 export interface MetaTag {
-    name: string;
+    name?: string;
     content: string;
+    property?: string;
 }
 
 export interface HeadData {
     title?: string;
     meta?: MetaTag[];
+    link?: { rel: string; href: string }[];
+    script?: { src: string; async?: boolean; defer?: boolean }[];
+    htmlAttributes?: { [key: string]: string };
 }
