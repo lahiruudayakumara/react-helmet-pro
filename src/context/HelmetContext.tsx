6c6ad80aecde797e90613ac04ebe9319b3837106
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2025-06-19 16:08:33 +0530

    refactor HelmetContext and HelmetProvider to include script and htmlAttributes in context type

--- src/context/HelmetContext.tsx
+++ src/context/HelmetContext.tsx
@@ -7,10 +7,14 @@ export interface HelmetContextType {
     title?: string;
     meta?: { name?: string; property?: string; content: string }[];
     link?: { rel: string; href: string }[];
+    script?: { src: string; async?: boolean; defer?: boolean }[];
+    htmlAttributes?: { [key: string]: string };
   }) => void;
   title?: string;
   meta?: { name?: string; property?: string; content: string }[];
   link?: { rel: string; href: string }[];
+  script?: { src: string; async?: boolean; defer?: boolean }[];
+  htmlAttributes?: { [key: string]: string };
 }
 
 export const HelmetContext = createContext<HelmetContextType | null>(null);
--- src/context/HelmetProvider.tsx
+++ src/context/HelmetProvider.tsx
@@ -3,15 +3,12 @@
 import React, { useMemo, useState } from "react";
 
 import { HelmetContext } from "./HelmetContext";
+import { HelmetData } from "../types";
 
 export const HelmetProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const [head, setHead] = useState<{
-    title?: string;
-    meta?: { name?: string; property?: string; content: string }[];
-    link?: { rel: string; href: string }[];
-  }>({});
+  const [head, setHead] = useState<HelmetData>({});
   const contextValue = useMemo(
     () => ({ setHead, title: head.title, meta: head.meta, link: head.link }),
     [setHead, head.title, head.meta, head.link]
